2878ce3eaa0fc3f40a22e7b2b0f7a2070393a456
AriefMade <ariepmade@gmail.com>
2024-04-16 04:56:48 +0800

    bahasa indonesia

--- kode prufer ke vertex.c
+++ kode prufer ke vertex.c
@@ -1,31 +1,25 @@
 //tangkas 
 #include <stdio.h>
-
-// Prints edges of tree represented by given Prufer code
 void printTreeEdges(int prufer[], int m)
 {
     int vertices = m + 2;
+
     int vertex_set[vertices];
 
-    // Initialize the array of vertices
     for (int i = 0; i < vertices; i++)
         vertex_set[i] = 0;
 
-    // Number of occurrences of vertex in code
+
     for (int i = 0; i < vertices - 2; i++)
         vertex_set[prufer[i] - 1] += 1;
 
     printf("\nHimpunan edge E(G) adalah :\n");
 //arief
-    // Find the smallest label not present in
-    // prufer[].
     int j = 0;
     for (int i = 0; i < vertices - 2; i++) {
         for (j = 0; j < vertices; j++) {
-            // If j+1 is not present in prufer set
             if (vertex_set[j] == 0) {
-                // Remove from Prufer set and print
-                // pair.
+
                 vertex_set[j] = -1;
                 printf("(%d, %d) ", j + 1, prufer[i]);
 
@@ -37,7 +31,6 @@ void printTreeEdges(int prufer[], int m)
     }
 
     j = 0;
-    // For the last element
     for (int i = 0; i < vertices; i++) {
         if (vertex_set[i] == 0 && j == 0) {
             printf("(%d, ", i + 1);
