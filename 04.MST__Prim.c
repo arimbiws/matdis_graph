d673101d7dd6e9fae72d20dce56614abc316d393
AriefMade <ariepmade@gmail.com>
2024-04-29 21:50:58 +0800

    final mst prim

--- 04.MST__Prim.c
+++ 04.MST__Prim.c
@@ -1,73 +1,69 @@
 #include <stdio.h>
 
-// Representasi matriks bobot graf
-int graf[6][6] = {{0, 10, 0, 30, 45, 0},
-                   {10, 0, 50, 0, 40, 25},
-                   {0, 50, 0, 0, 35, 15},
-                   {30, 0, 0, 0, 0, 20},
-                   {45, 40, 35, 0, 0, 55},
-                   {0, 25, 15, 20, 55, 0}};
+struct sisi {
+    int asal;
+    int tujuan;
+    int berat;
+};
 
-// Representasi array edge dengan dua baris, masing-masing untuk vertex awal dan akhir
-int sisi[2][10] = {{0, 2, 3, 1, 0, 2, 1, 0, 1, 4}, 
-                    {1, 5, 5, 5, 3, 4, 4, 4, 3, 5}};
+struct sisi edges[] = {
+    {0, 1, 10},
+    {0, 3, 15},
+    {1, 2, 20},
+    {1, 4, 13},
+    {2, 5, 12},
+    {3, 4, 11},
+    {3, 5, 16},
+    {4, 2, 5},
+    {4, 5, 21}
+};
 
-// Representasi matriks MST dengan default nilai nol
-int MST[6][6] = {{0}};
+int dikunjungi[9];
+int bobotMST = 0;
 
-// Jumlah edge yang akan diproses
-int jumlah_sisi = sizeof(sisi[0]) / sizeof(sisi[0][0]);
+void printGraph() {
+    printf("Graf:\n");
+    for (int i = 0; i < sizeof(edges) / sizeof(edges[0]); i++) {
+        printf("(sisi %d - %d) \tbobot : %d\n", edges[i].asal, edges[i].tujuan, edges[i].berat);
+    }
+    printf("\n ===== Hasil =====\n");
+    printf("Total MST: %d\n", bobotMST);
+}
 
-// Inisialisasi variabel counter edge dan total bobot MST
-int hitung_sisi = 1, total_bobot = 0;
-
-void resetArray(int arr[], int ukuran) {
-    for (int i = 0; i < ukuran; i++) {
-        arr[i] = 0;
+// Fungsi untuk mencari sisi terkecil yang menghubungkan simpul dalam MST
+void cariSisiTerkecil(int *v, int *vn) {
+    int min = 999;
+    for (int i = 0; i < sizeof(edges) / sizeof(edges[0]); i++) {
+        if ((dikunjungi[edges[i].asal] && !dikunjungi[edges[i].tujuan]) ||
+            (!dikunjungi[edges[i].asal] && dikunjungi[edges[i].tujuan])) {
+            if (edges[i].berat < min) {
+                min = edges[i].berat;
+                *v = edges[i].asal;
+                *vn = edges[i].tujuan;
+            }
+        }
     }
 }
 
 int main() {
-    // Array untuk tracking vertex yang telah dikunjungi
-    int dikunjungi[6] = {0};
-
-    for (int sisi_ke = 0; sisi_ke < jumlah_sisi && hitung_sisi <= 5; sisi_ke++) {
-        int vertex_awal = sisi[0][sisi_ke], vertex_akhir = sisi[1][sisi_ke];
-
-        resetArray(dikunjungi, 6);
-
-        // Copy nilai dari MST ke dikunjungi untuk vertex awal
-        for (int j = 0; j < 6; j++) {
-            dikunjungi[j] = MST[vertex_awal][j];
-        }
-
-        int cek = 0;
-        while (cek < 6) {
-            for (int j = 0; j < 6; j++) {
-                if (dikunjungi[j] == 1 && MST[j][j] == 0) {
-                    dikunjungi[j] = 1;
-                    for (int k = 0; k < 6; k++) {
-                        if (MST[j][k] == 1) {
-                            dikunjungi[k] = 1;
-                        }
-                    }
-                }
-            }
-            cek++;
-        }
-
-        if (dikunjungi[vertex_akhir] == 0) {
-            MST[vertex_awal][vertex_akhir] = 1;
-            MST[vertex_akhir][vertex_awal] = 1;
-            total_bobot += graf[vertex_awal][vertex_akhir];
-
-            printf("\n%d - %d bobot %d total MST = %d", vertex_awal, vertex_akhir, graf[vertex_awal][vertex_akhir], total_bobot);
-
-            graf[vertex_awal][vertex_akhir] = 0;
-            graf[vertex_akhir][vertex_awal] = 0;
-            hitung_sisi++;
-        }
+    for (int i = 0; i < 6; i++) {
+        dikunjungi[i] = 0;
     }
 
+    printGraph();
+
+    dikunjungi[0] = 1;
+    int k = 1;
+
+    while (k < 6) {
+        int v, vn;
+        cariSisiTerkecil(&v, &vn);
+        printf("%d - %d, Bobot: %d\n", v, vn, edges[vn].berat);
+        bobotMST += edges[vn].berat;
+        dikunjungi[vn] = 1;
+        k++;
+    }
+
+    printf("\nBobot MST tersebut sebesar : %d\n", bobotMST);
     return 0;
 }
--- mstPrim.c
+++ mstPrim.c
@@ -1,71 +1,96 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-struct sisi {
+
+
+#define V 6
+#define E 9
+
+struct Sisi {
     int asal;
     int tujuan;
     int berat;
 };
 
-struct sisi edges[] = {
+struct Sisi edges[E] = {
     {0, 1, 10},
-    {0, 3, 30},
-    {0, 4, 45},
-    {1, 2, 50},
-    {1, 5, 25},
-    {2, 4, 35},
-    {2, 5, 15},
-    {3, 5, 20},
-    {4, 5, 55}
+    {0, 3, 15},
+    {1, 2, 20},
+    {1, 4, 13},
+    {2, 5, 12},
+    {3, 4, 11},
+    {3, 5, 16},
+    {4, 2, 5},
+    {4, 5, 21}
 };
 
-int dikunjungi[6];
+int dikunjungi[V];
 int bobotMST = 0;
 
-void printGraph() {
-    printf("Graf:\n");
-    for (int i = 0; i < sizeof(edges) / sizeof(edges[0]); i++) {
-        printf("(sisi %d = %d - %d) bobot : %d\n", i+1, edges[i].asal, edges[i].tujuan, edges[i].berat);
-    }
-    printf("\n ===== Hasil =====\n");
-}
+// 2
 
-// Fungsi untuk mencari sisi terkecil yang menghubungkan simpul dalam MST
-void cariSisiTerkecil(int *v, int *vn) {
+int cariSimpulDenganBobotMin(int bobot[], bool dalamMST[]) {
     int min = 999;
-    for (int i = 0; i < 6; i++) {
-        if (dikunjungi[i]) {
-            for (int j = 0; j < sizeof(edges) / sizeof(edges[0]); j++) {
-                if (!dikunjungi[edges[j].tujuan] && edges[j].berat < min) {
-                    min = edges[j].berat;
-                    *v = edges[j].asal;
-                    *vn = edges[j].tujuan;
-                }
+    int indeksMin = -1;
+
+    for (int v = 0; v < V; v++) {
+        if (dalamMST[v] == false && bobot[v] < min) {
+            min = bobot[v];
+            indeksMin = v;
+        }
+    }
+    return indeksMin;
+}
+void bangunMST() {
+    int parent[V];
+    int bobot[V];
+    bool dalamMST[V];
+    for (int i = 0; i < V; i++) {
+        bobot[i] = 999;
+        dalamMST[i] = false;
+    }
+    bobot[0] = 0;
+    parent[0] = -1;
+    for (int count = 0; count < V - 1; count++) {
+        int u = cariSimpulDenganBobotMin(bobot, dalamMST);
+        dalamMST[u] = true;
+
+// 3
+
+        for (int v = 0; v < E; v++) {
+            if ((edges[v].asal == u || edges[v].tujuan == u) 
+            && dalamMST[edges[v].tujuan] == false 
+            && edges[v].berat < bobot[edges[v].tujuan]) {
+                parent[edges[v].tujuan] = u;
+                bobot[edges[v].tujuan] = edges[v].berat;
             }
         }
     }
-}
 
-// Fungsi untuk membangun Minimum Spanning Tree
-void bangunMST() {
-    dikunjungi[0] = 1;
-    int k = 1;
-
-    while (k < 6) {
-        int v, vn;
-        cariSisiTerkecil(&v, &vn);
-        printf("%d - %d\n", v, vn);
-        bobotMST += edges[vn].berat;
-        dikunjungi[vn] = 1;
-        k++;
+    for (int i = 1; i < V; i++) {
+        printf("%d - %d, Bobot: %d\n", parent[i], i, bobot[i]);
+        bobotMST += bobot[i];
     }
 }
+
+void printGraph() {
+    printf("Graf:\n");
+    for (int i = 0; i < E; i++) {
+        printf("(sisi %d - %d) \tbobot : %d\n", edges[i].asal, edges[i].tujuan, edges[i].berat);
+    }
+    printf("\n===== Hasil =====\n");
+
+    bangunMST();
+}
+
+// 1 
 
 int main() {
-    for (int i = 0; i < 6; i++) {
+    for (int i = 0; i < V; i++) {
         dikunjungi[i] = 0;
     }
+
     printGraph();
-    bangunMST();
 
     printf("\nBobot MST tersebut sebesar : %d\n", bobotMST);
     return 0;
